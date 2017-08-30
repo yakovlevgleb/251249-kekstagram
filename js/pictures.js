6ee9193d2300ef3fd03debddcea2992956652a1a
Yakovlev Gleb <brin@ya.ru>
2017-08-30 16:57:04 +0500

    Добавил валидацию хэштэга и сброс

--- js/pictures.js
+++ js/pictures.js
@@ -115,47 +115,117 @@ renderPicture(pictureDescription);
 document.querySelector(selectors.upload).classList.add('hidden');
 renderGallery(galleryOverlay);
 
-var elementName;
+var uploadForm = document.querySelector('.upload-form');
+var uploadEffect = document.querySelector('.upload-effect');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFormHashtags = document.querySelector('.upload-form-hashtags');
+var effectImagePreview = document.querySelector('.effect-image-preview');
 
-document.querySelector('.upload-effect').addEventListener('click', function (evt) {
-  document.querySelector('.effect-image-preview').classList.remove('effect-' + elementName);
+var uploadResizeControlsValue = document.querySelector('.upload-resize-controls-value');
+var uploadResizeControlsButtonDec = document.querySelector('.upload-resize-controls-button-dec');
+var uploadResizeControlsButtonInc = document.querySelector('.upload-resize-controls-button-inc');
+
+
+uploadEffect.addEventListener('click', function (evt) {
   var target = evt.target;
   var elementStyle = target.parentNode.getAttribute('for').replace('upload-', '');
-  var imagePreviewUploadOverlay = document.querySelector('.upload-overlay').querySelector('.effect-image-preview');
+  addFilterClass(elementStyle);
+});
+
+var addFilterClass = function (elementStyle) {
+  var imagePreviewUploadOverlay = uploadOverlay.querySelector('.effect-image-preview');
   imagePreviewUploadOverlay.classList = '';
   imagePreviewUploadOverlay.classList.add('effect-image-preview');
   imagePreviewUploadOverlay.classList.add(elementStyle);
-});
+};
 
-document.querySelector('.upload-resize-controls-button-dec').addEventListener('click', function () {
-  var persentValue = document.querySelector('.upload-resize-controls-value').value;
+uploadResizeControlsButtonDec.addEventListener('click', function () {
+  var persentValue = uploadResizeControlsValue.value;
   var intValue = parseInt(persentValue, 10);
   intValue -= 25;
-  if (intValue < 25) {
-    intValue = 25;
-  }
-  document.querySelector('.upload-resize-controls-value').value = intValue + '%';
-  intValue = intValue / 100;
-  var imagePreview = document.querySelector('.effect-image-preview');
-  imagePreview.style.transform = 'scale(' + intValue + ')';
+  intValue = validePersentValue(intValue);
+  uploadResizeControlsValue.value = intValue + '%';
+  changeScale(intValue);
 });
 
-document.querySelector('.upload-resize-controls-button-inc').addEventListener('click', function () {
-  var persentValue = document.querySelector('.upload-resize-controls-value').value;
+uploadResizeControlsButtonInc.addEventListener('click', function () {
+  var persentValue = uploadResizeControlsValue.value;
   var intValue = parseInt(persentValue, 10);
   intValue += 25;
-  if (intValue > 100) {
-    intValue = 100;
-  }
-  document.querySelector('.upload-resize-controls-value').value = intValue + '%';
-  intValue = intValue / 100;
-  var imagePreview = document.querySelector('.effect-image-preview');
-  imagePreview.style.transform = 'scale(' + intValue + ')';
+  intValue = validePersentValue(intValue);
+  uploadResizeControlsValue.value = intValue + '%';
+  changeScale(intValue);
 });
 
-document.querySelector('.upload-resize-controls-value').addEventListener('change', function () {
-  var persentValue = document.querySelector('.upload-resize-controls-value').value;
-  var intValue = parseInt(persentValue, 10) / 100;
-  var imagePreview = document.querySelector('.effect-image-preview');
-  imagePreview.style.transform = 'scale(' + intValue + ')';
+var validePersentValue = function (intValue) {
+  if (intValue <= 25) {
+    intValue = 25;
+  } else if (intValue > 100) {
+    intValue = 100;
+  }
+  return intValue;
+};
+
+var changeScale = function (value) {
+  value = value / 100;
+  effectImagePreview.style.transform = 'scale(' + value + ')';
+};
+
+uploadResizeControlsValue.addEventListener('change', function () {
+  var persentValue = uploadResizeControlsValue.value;
+  var index = parseInt(persentValue, 10) / 100;
+  effectImagePreview.style.transform = 'scale(' + index + ')';
+});
+
+uploadFormHashtags.addEventListener('change', function (evt) {
+  onHashtagsInput(evt);
+});
+
+function hashtagsValid(evt, noSharp, isRepeated, isLong, tooMuch) {
+  var target = evt.target;
+
+  if (isRepeated) {
+    target.setCustomValidity('Зэштэги не должны повторяться');
+  } else if (noSharp) {
+    target.setCustomValidity('Хэштэги должны начинаться с \'#\' разделя их \' \'');
+  } else if (isLong) {
+    target.setCustomValidity('Хэштэг не может состоять из более 20 символов');
+  } else if (tooMuch) {
+    target.setCustomValidity('Количество хэштэгов не должно превышать 5');
+  } else {
+    target.setCustomValidity('');
+  }
+}
+
+function onHashtagsInput(evt) {
+  var target = evt.target;
+  var valueArray = target.value.split(' ');
+
+  var noSharp = false;
+  var isRepeated = false;
+  var isLong = false;
+  var tooMuch = false;
+
+  var testArr = [];
+  tooMuch = valueArray.length > 5;
+
+  for (var i = 0; i < valueArray.length; i++) {
+    isLong = valueArray[i].length > 20;
+    noSharp = valueArray[i].charAt(0) !== '#';
+
+    if (testArr.indexOf(valueArray[i]) === -1) {
+      testArr.push(valueArray[i]);
+    } else {
+      isRepeated = true;
+    }
+  }
+
+  hashtagsValid(evt, noSharp, isRepeated, isLong, tooMuch);
+}
+
+uploadForm.addEventListener('submit', function (evt) {
+  document.querySelector('#upload-effect-none').setAttribute('checked');
+  uploadFormHashtags.value = '';
+  document.querySelector('.upload-form-description').value = '';
+  evt.preventDefault();
 });
